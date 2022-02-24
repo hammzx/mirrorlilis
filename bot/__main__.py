333d3dfabd59b747141121627deb291a9677952c
hammzx <95368225+hammzx@users.noreply.github.com>
2022-02-24 08:07:17 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -58,7 +58,7 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("𝐎 𝐖 𝐍 𝐄 𝐑", "https://t.me/xxyznxx")
+    buttons.buildbutton("𝐎 𝐖 𝐍 𝐄 𝐑", "https://t.me/mrmellaada")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
